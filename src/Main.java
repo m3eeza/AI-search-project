f4c9f4ff4ff5e8636c6f496b0756ffe8b83eb0e4
Abdullah Kady <abdullahkady@gmail.com>
2019-10-08 20:54:27 +0200

    Add stuff

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	
 
-	public static char[][] gridParser(String grid){
-		int ns = grid.split(";")[3].split(",").length; //number of stones
+	public static char[][] gridParser(String grid) {
+		int ns = grid.split(";")[3].split(",").length; // number of stones
 		int nw = grid.split(";")[4].split(",").length; // number of warriors
 		Scanner sc = new Scanner(grid);
 		sc.useDelimiter(";|,");
@@ -16,16 +15,16 @@ public class Main {
 		int tx = sc.nextInt();
 		int ty = sc.nextInt();
 		char[][] map = new char[m][n];
-		for (char[] row: map)
-		    Arrays.fill(row, 'E');
+		for (char[] row : map)
+			Arrays.fill(row, 'E');
 		map[ix][iy] = 'I';
 		map[tx][ty] = 'T';
-		for (int i = 0; i < ns; i+=2) {
+		for (int i = 0; i < ns; i += 2) {
 			int six = sc.nextInt();
 			int siy = sc.nextInt();
 			map[six][siy] = 'S';
 		}
-		for (int i = 0; i < nw; i+=2) {
+		for (int i = 0; i < nw; i += 2) {
 			int wix = sc.nextInt();
 			int wiy = sc.nextInt();
 			map[wix][wiy] = 'W';
@@ -33,20 +32,30 @@ public class Main {
 		sc.close();
 		return map;
 	}
+
 	public static String solve(String grid, String strategy, boolean visualize) {
 		String solution = "";
 		return solution;
-		
+
 	}
+
 	public static void main(String[] args) {
 		String grid = "5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3";
-		char [][] map = gridParser(grid);
+		char[][] map = gridParser(grid);
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map.length; j++) {
-				System.out.print(map[i][j]+" ");
+				System.out.print(map[i][j] + " ");
 			}
 			System.out.println("");
 		}
+		EndGame e = new EndGame(new State(map, 0));
+
+		// DFS dfs = new DFS();
+		// Node solution = dfs.solve(e);
+		BFS bfs = new BFS();
+		Node solution = bfs.solve(e);
+		System.out.println(solution);
+		System.out.println(solution.pathToString());
 	}
 
 }
--- src/SearchProblem.java
+++ src/SearchProblem.java
@@ -1,9 +1,13 @@
 import java.util.Collection;
 
 public interface SearchProblem {
-	public Object getInitialState();
+    public Object getInitialState();
+
     public boolean isGoal(Object state);
-    public Collection<Object> getActions(Object state);
-    public Object getNextState(Object state,Object action);
-    public int getStepCost(Object start, Object action, Object dest);
+
+    public Collection<String> getAllowedActions(Object state);
+
+    public Object getNextState(Object state, String action);
+
+    public int getStepCost(Object start, String action, Object dest);
 }
